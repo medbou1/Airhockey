278d483aa63f710c1b7b6368e29607dc6ac559b5
GuerfelMohamed <mohamed.guerfel@ecl19.ec-lyon.fr>
2020-03-24 10:59:12 +0100

    parite serveur

---
+++ bin/bullet.js
@@ -0,0 +1,177 @@
+
+class object{
+	constructor(X,Y){
+		this.x=X;
+		this.y=Y;
+	}
+	updatePosition(spdX,spdY){
+		this.x+=spdX;
+		this.y+=spdY;
+
+		this.pressingRight = false;
+		this.pressingLeft = false;
+		this.pressingUp = false;
+		this.pressingDown = false;
+
+		this.width=26;
+		this.height=120
+	}
+}
+class Bullet extends object{
+	constructor(angle,X,Y,playerList){
+	 super(X,Y);
+	 this.playerList=playerList
+	 if(Math.random()<=0.5)
+	 this.spdX = -3;
+     else
+	 this.spdX = 3;
+     if(Math.random()<=0.5)
+	 this.spdY = -3;
+     else
+	 this.spdY = 3;
+
+	 this.r = 38;
+	}
+	player_bullet_collision(number_player){
+		
+		let left = this.x ,
+                        right = this.x + this.r,
+                        top = this.y ,
+                        bottom = this.y +this.r;
+                    
+                let pleft = this.playerList[number_player].x - this.playerList[number_player].width / 2,
+                        pright = this.playerList[number_player].x + this.playerList[number_player].width / 2,
+                        ptop = this.playerList[number_player].y - this.playerList[number_player].height / 2,
+                        pbottom = this.playerList[number_player].y + this.playerList[number_player].height / 2;
+		
+                    
+
+		
+		if (number_player==0 && top>ptop && bottom<pbottom && right>pleft && this.x+this.r/2<pleft)
+                    {
+                        this.spdX -= 1;
+						
+                    }
+		else if (number_player==0 && top>ptop && bottom<pbottom && left<pright && this.x+this.r/2>pright && this.spdX<0 )
+                    {
+                        this.spdX *= -1;
+			            this.spdX += 1;
+						
+                    }
+		else if(number_player==0 && right> pleft && pright> left) 
+                    {   if (bottom>ptop && ptop>top)
+		        {	if( this.spdY>0){
+					this.spdY *= -1;
+					this.spdY = this.spdY-1;
+				}
+				if(this.x+this.r/2>pright && this.spdX<0 ){
+					this.spdX *= -1;
+					this.spdX += 1;
+				}
+			}
+			else if(top<pbottom && pbottom<bottom)
+			{	if(this.spdY<0){
+					this.spdY *= -1;
+					this.spdY = this.spdY+1;
+				}
+			 	if(this.x+this.r/2>pright && this.spdX<0 ){
+					this.spdX *= -1;
+					this.spdX += 1;
+			 	}
+			}
+                    }
+		else if (number_player==1 && top>ptop && bottom<pbottom && left<pright && this.x+this.r/2>pright)
+                    {
+                        this.spdX += 1;
+						
+                    }
+		else if (number_player==1 && top>ptop && bottom<pbottom && right>pleft && this.x+this.r/2<pleft && this.spdX>0)
+                    {
+                        this.spdX *= -1;
+			            this.spdX = this.spdX-1;
+						
+                    }
+		else if(number_player==1 && left< pright && pleft<right)
+                {
+                        if (bottom>ptop && ptop>top )
+                        {	if( this.spdY>0){
+					this.spdY *= -1;
+					this.spdY = this.spdY-1;
+				}
+				if(this.x+this.r/2<pleft && this.spdX>0 ){
+					this.spdX *= -1;
+					this.spdX = this.spdX-1;
+				}
+			}
+			else if(top<pbottom && pbottom<bottom)
+			{	if( this.spdY<0){
+					this.spdY *= -1;
+					this.spdY = this.spdY+1;
+				}
+			 	if(this.x+this.r/2<pleft && this.spdX>0 ){
+					this.spdX *= -1;
+					this.spdX = this.spdX-1;
+				}
+			}
+						
+                }
+		if(this.spdX>20)
+			this.spdX = 20;
+		else if(this.spdX< -20)
+			this.spdX = -20;
+		if(this.spdY>20)
+			this.spdY = 20;
+		else if(this.spdY< -20)
+			this.spdY = -20;		    
+	   }
+
+	update(){
+		/*les équations de la positon de la balle*/
+	 if(this.y + this.r>650 && this.spdY>0){
+		 this.spdY *= -1;
+		 
+	 }
+	 else if(this.y /*- this.r*/<0 && this.spdY<0){
+		 this.spdY *= -1;
+		 
+	 }
+	 
+	 if(this.x + this.r>1000){
+		 if(Math.random()<=0.5)
+			 this.spdX = -3;
+		 else
+			 this.spdX = 3;
+		 if(Math.random()<=0.5)
+			 this.spdY = -3;
+		 else
+			 this.spdY = 3;
+		 this.y = 325-this.r;
+		 this.x = 500-this.r;
+		this.playerList[0].score++;
+		 
+	 }
+	 else if(this.x + this.r<0){
+		 if(Math.random()<=0.5)
+			 this.spdX = -3;
+		 else
+			 this.spdX = 3;
+		 if(Math.random()<=0.5)
+			 this.spdY = -3;
+		 else
+			 this.spdY = 3;
+		 this.y = 325-this.r;
+		 this.x = 500-this.r;
+		 this.playerList[1].score++;
+		 
+	 }
+	 else  {
+		 for(let i in this.playerList){			
+			 this.player_bullet_collision(this.playerList[i].id);
+		 }	 
+	 }
+	 super.updatePosition(this.spdX,this.spdY);
+	 return {x:this.x,y:this.y,r:this.r};
+ }
+}
+
+module.exports=Bullet;
---
+++ bin/players.js
@@ -0,0 +1,96 @@
+
+class object{
+	constructor(X,Y){
+		this.x=X;
+		this.y=Y;
+	}
+	updatePosition(spdX,spdY){
+		this.x+=spdX;
+		this.y+=spdY;
+
+		this.pressingRight = false;
+		this.pressingLeft = false;
+		this.pressingUp = false;
+		this.pressingDown = false;
+
+		this.width=26;
+		this.height=120
+	}
+}
+
+class player extends object{
+	constructor(id,X,Y){
+		super(X,Y);
+		this.id=id;
+		this.score=0;
+		this.playerName="";
+
+	}
+	getScore(){
+		return this.score;
+	}
+	updateSpd(maxSpd,width,height){//maxSpd modifiable
+		let spdX=0;
+		let spdY=0;
+		if(this.pressingRight && ((this.id==0 && this.x+width/2<(1000/2-48)) || (this.id==1 && this.x+width/2<1000))){
+			
+				spdX =maxSpd;
+		}
+		else if(this.pressingLeft && ((this.id==0 && this.x-width/2>0) || (this.id==1 && this.x-width/2>1000/2+48)))
+			spdX = -maxSpd;
+		else 
+			spdX = 0;
+	 
+		if(this.pressingUp && this.y-height/2>0)
+			spdY = -maxSpd;
+		else if(this.pressingDown && this.y+height/2<650)
+			spdY = maxSpd;
+		else
+			spdY = 0;
+		return [spdX,spdY];
+	}
+	update(){
+		let aux=this.updateSpd(8,26,120);
+       super.updatePosition(aux[0],aux[1]);
+	}
+}
+
+class Players{
+	constructor() {
+		this.playerList=[];
+	}
+	addPlayer(id,X,Y){
+		let aux=new player(id,X,Y);
+		this.playerList[id]=aux;
+		return aux;
+	}
+	onConnect(socket,X,Y){
+		let player=this.addPlayer(socket.id,X,Y);
+		socket.on('keyPress',function(data){
+			if(data.inputId === 'left')
+				player.pressingLeft = data.state;
+			else if(data.inputId === 'right')
+				player.pressingRight = data.state;
+			else if(data.inputId === 'up')
+				player.pressingUp = data.state;
+			else if(data.inputId === 'down')
+				player.pressingDown = data.state;
+			
+		});
+
+	}
+	onDisconnect(socket){
+      delete this.playerList[socket.id];
+	}
+	update(){
+		for(let i in this.playerList){
+			this.playerList[i].update();
+		}
+		return this.playerList;
+	}
+}
+
+
+
+
+module.exports=Players;
