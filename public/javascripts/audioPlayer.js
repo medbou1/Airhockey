bcaf90e6018b7578a6057da10ddd08a2d7af1fe6
medbou1 <36161107+medbou1@users.noreply.github.com>
2020-03-28 15:19:53 +0100

    audio list modified and integrated
    the buttons does not show up for some reason please fix!!

---
+++ public/javascripts/audioPlayer.js
@@ -0,0 +1,61 @@
+function _(id){
+	return document.getElementById(id);
+}
+function audioApp(){
+	var audio = new Audio();
+	var audio_folder = "/audio/";
+	var audio_ext = ".mp3";
+	var audio_index = 1;
+	var is_playing = false;
+	var playingtrack;
+	var trackbox = _("trackbox");
+	var tracks = {
+	    "track1":["Bass", "bass"],
+		"track2":["Sax", "sax"],
+		"track3":["Piano", "piano"]
+	};
+	for(var track in tracks){
+		var tb = document.createElement("div");
+		var pb = document.createElement("button");
+		var tn = document.createElement("div");
+		tb.className = "trackbar";
+		pb.className = "playbutton";
+		tn.className = "trackname";
+		tn.innerHTML = audio_index+". "+tracks[track][0];
+		pb.id = tracks[track][1];
+		pb.addEventListener("click", switchTrack);
+		tb.appendChild(pb);
+		tb.appendChild(tn);
+		trackbox.appendChild(tb);
+		audio_index++;
+	}
+	audio.addEventListener("ended",function(){
+	    _(playingtrack).style.background = "images/ak_playbtn.png";
+		playingtrack = "";
+		is_playing = false;
+	});
+	function switchTrack(event){
+		if(is_playing){
+		    if(playingtrack != event.target.id){
+			    is_playing = true;
+				_(playingtrack).style.background = "/images/ak_playbtn.png";
+			    event.target.style.background = "/images/ak_pausebtn.png";
+			    audio.src = audio_folder+event.target.id+audio_ext;
+	            audio.play();
+			} else {
+			    audio.pause();
+			    is_playing = false;
+				event.target.style.background = "/images/ak_playbtn.png";
+			}
+		} else {
+			is_playing = true;
+			event.target.style.background = "/images/ak_pausebtn.png";
+			if(playingtrack != event.target.id){
+				audio.src = audio_folder+event.target.id+audio_ext;
+			}
+	        audio.play();
+		}
+		playingtrack = event.target.id;
+	}
+}
+window.addEventListener("load", audioApp);
